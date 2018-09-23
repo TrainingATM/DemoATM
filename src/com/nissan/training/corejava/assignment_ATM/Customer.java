a469d5e2dc8e8215c0e6a128d000ca515b395ab3
MOHITANAND <NDH00157@Nmcorp.Nissan.Biz>
2018-09-23 12:24:02 +0530

    Added declarations of Customer class

---
+++ src/com/nissan/training/corejava/assignment_ATM/Customer.java
@@ -0,0 +1,14 @@
+package com.nissan.training.corejava.assignment_ATM;
+import java.util.Calendar;
+
+public class Customer {
+	private String name;
+	private String address;
+	private Calendar dob;
+	private String cardNumber;
+	private int pin;
+	
+	boolean verifyPassword(final String passwd) {
+		
+	}
+}
