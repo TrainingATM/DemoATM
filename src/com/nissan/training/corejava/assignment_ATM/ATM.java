1072a779c5d81ca31696023f6a506d015627265f
SANATH <NDH00198@Nmcorp.Nissan.Biz>
2018-09-23 13:52:50 +0530

    Added skeleton code for ATM and ATMTransaction

---
+++ src/com/nissan/training/corejava/assignment_ATM/ATM.java
@@ -0,0 +1,18 @@
+package com.nissan.training.corejava.assignment_ATM;
+
+public class ATM {
+	
+	private String location;
+	private String managedBy;
+	
+	public boolean identify(String cardNumber, String pin)
+	{
+		
+	}
+	
+	public ATMTransactions[] transactions()
+	{
+		
+	}
+
+}
---
+++ src/com/nissan/training/corejava/assignment_ATM/ATMTransactions.java
@@ -0,0 +1,16 @@
+package com.nissan.training.corejava.assignment_ATM;
+
+import java.util.Date;
+
+public class ATMTransactions {
+	private int transaction_ID;
+	private Date date;
+	private String type;
+	private int amount;
+	private int postBalance;
+	
+	public void modifier()
+	{
+		
+	}
+}
